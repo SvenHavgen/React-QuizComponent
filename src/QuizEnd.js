eb5d75a1a2a8286c4f38e62e7447677239b80196
Sven Havgen <sven_havgen@hotmail.com>
2018-10-19 12:04:54 +1100

    Update onclick to onClick

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component {
-
     handleResetClick() {
         this.props.resetClickHandler()
     }
@@ -9,7 +8,7 @@ class QuizEnd extends Component {
         return (
             <div>
                 <p>Thanks for playing!</p>
-                <a href='' onclick={this.handleResetClick.bind(this)}>Reset Quiz</a>
+                <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
             </div>
             )
     }
